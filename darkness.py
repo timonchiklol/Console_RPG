5651c8a52b4b6cedc59872bf5ea2544e2bfe6537
timasukhovm <timasukhovm@gmail.com>
2025-01-18 16:31:11 +0100

    base

--- darkness.py
+++ darkness.py
@@ -1,7 +1,34 @@
+# imports
+from random import randint
 
+#база, не трогать
+start = False
+gold = 0
+magic_1lvl = 0
+magic_2lvl = 0
+level = 0
+health_points = 0
+damage = randint(1,10)
 
 # User_place это место на котором сейчас находится игрок
 User_place = "menu"
+
+def race():
+    race = input("Race options: Human, Elf ")
+    if race == "Human":
+        health_points = 15
+        level = 1
+        gold = 5
+        damage = randint(2,7)
+        
+    elif race == "Elf":
+        health_points = 10
+        level = 1
+        gold = 5
+        damage = randint(1,12)
+
+    else:
+         print("I dont understand:(")
 
 
 
@@ -12,6 +39,7 @@ def menu():
     menu_input = input()
     if menu_input == "start":
         return "вот тут надо чтобы нейросетка начинала генерить"
+        start = True
     elif menu_input == "exit":
         exit()
     elif menu_input == "about":
@@ -20,3 +48,6 @@ def menu():
          print("I dont understand:(")
 while True:
     menu()
+    if start == True:
+        race()
+        
