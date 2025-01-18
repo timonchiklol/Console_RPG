dbd115621d431b2f2cf0bb5ec9bfebc3ebe0d2be
timasukhovm <timasukhovm@gmail.com>
2025-01-18 15:43:58 +0100

    menu

--- darkness.py
+++ darkness.py
@@ -1,1 +1,17 @@
-print("hello console")
+# User_place это место на котором сейчас находится игрок
+User_place = "menu"
+
+def menu():
+    print("start")
+    print("about") 
+    print("exit")
+    menu_input = input()
+    if menu_input == "start":
+        return "вот тут надо чтобы нейросетка начинала генерить"
+    elif menu_input == "exit":
+        exit()
+    elif menu_input == "about":
+        print("Nothing is here yet")
+    else:
+         print("I dont understand:(")
+menu()
